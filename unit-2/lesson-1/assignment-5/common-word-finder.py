4451ae51c04a43bffc851950f1f10fa634b844d7
perep797 <perep797@newschool.edu>
2024-10-12 14:32:02 -0400

    adding dictionary

---
+++ unit-2/lesson-1/assignment-5/common-word-finder.py
@@ -0,0 +1,7 @@
+Solaris = open('dictionary.txt') #I didn't want to have the text in the same file as my code 
+dictionary = {Solaris}
+print(dictionary)
+# dictionary["the"] = "the"
+# dictionary["and"] = "and"
+# dictionary["I"] = "I"
+# dictionary["of"] = "of"
